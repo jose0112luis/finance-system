c9a6cf9874a24ee53bedbb06122eb110f3511a65
Jose Luis <jose0112luis@hotmail.com>
2023-01-09 21:49:12 -0500

    Implementando variables de entorno a la app con .env de la librería dotenv

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,12 +1,15 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5433,
-  username: 'root',
-  password: 'jlpv123',
-  database: 'finance_db',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/database/migrations/*.js'],
 };
