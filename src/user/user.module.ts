c6900d2a9fa8609705cb8661cd0fbd2fd2cf06bb
Jose Luis <jose0112luis@hotmail.com>
2023-01-09 21:30:48 -0500

    Actualizar Module de user con el controller, service y entity de user + typeorm

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
+import { User } from '../database/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
