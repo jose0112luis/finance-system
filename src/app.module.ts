6ed8a05e7a69b116880788f38c8fc79b0555d595
Jose Luis <jose0112luis@hotmail.com>
2023-01-09 22:30:18 -0500

    Configuración (@nestjs/config) y validación (joi) para las variables de entorno en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { dataSourceOptions } from './database/dataSource';
+import config from './database/config';
 
+@Global()
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USERNAME: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DATABASE: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [TypeOrmModule],
--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT),
+  port: parseInt(process.env.POSTGRES_PORT, 10),
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
