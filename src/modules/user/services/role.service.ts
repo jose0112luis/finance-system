7adc6e9faa191b3756b7a5c63f93f02011f69c35
Jose Luis <jose0112luis@hotmail.com>
2023-01-10 22:36:50 -0500

    Service y Controller de Role

--- src/modules/user/services/role.service.ts
+++ src/modules/user/services/role.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Role } from '../../../database/entities/role.entity';
+import { CreateRoleDto, UpdateRoleDto } from '../dto/role.dto';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(@InjectRepository(Role) private roleRepo: Repository<Role>) {}
+
+  findAll() {
+    return this.roleRepo.find();
+  }
+
+  async findOne(id: number) {
+    const role = await this.roleRepo.findOne({ where: { id } });
+    if (!role) {
+      throw new NotFoundException(`User ${id} not found`);
+    }
+    return role;
+  }
+
+  async create(data: CreateRoleDto) {
+    const newRole = this.roleRepo.create(data);
+    return this.roleRepo.save(newRole);
+  }
+
+  async update(id: number, data: UpdateRoleDto) {
+    const role = await this.roleRepo.findOne({ where: { id } });
+    this.roleRepo.merge(role, data);
+    return this.roleRepo.save(role);
+  }
+
+  remove(id: number) {
+    return this.roleRepo.delete(id);
+  }
+}
